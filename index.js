df22962828f131a6bc4790ba8b7061ed49bd2322
Kelliann Lafferty <kell10192@gmail.com>
2019-05-02 03:57:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-onst musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians,instruments) {
