e648d590ae0272b59d0c048205af4053d98e0072
Kelliann Lafferty <kell10192@gmail.com>
2019-05-02 04:03:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ function johnLennonFacts () {
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"]
 var array = []
-var i = 0
 while (array.length < facts.length)
 array.push(facts[i] + "!!!")
-i++
 return array
 }
 
