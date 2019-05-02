bb902c7ca8306bf873331d2865ce7a1f436b1b89
Kelliann Lafferty <kell10192@gmail.com>
2019-05-02 04:02:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ function johnLennonFacts () {
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]
+  "He hated the sound of his own voice"]
 var array = []
 var i = 0
 while (array.length < facts.length)
